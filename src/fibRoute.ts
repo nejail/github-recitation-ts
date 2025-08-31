ba2e1f2458d2654b7d7fc6db571a3e93eaa45c9a
“Fahad” <fahadabd@andrew.cmu.edu>
2025-08-31 18:52:53 +0300

    fixed fib3

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,6 +1,6 @@
 // Endpoint for querying the fibonacci numbers
 
-const fibonacci = require("./fib");
+const fibonacci = require("./fib.ts");
 
 export default (req, res) => {
   const { num } = req.params;
