d2bfff1003006e7987e54727a3c16cdb07a5579d
“Fahad” <fahadabd@andrew.cmu.edu>
2025-08-31 18:51:25 +0300

    fixed fib3

--- src/fib.ts
+++ src/fib.ts
@@ -1,10 +1,10 @@
 // util function that computes the fibonacci numbers
 module.exports = function fibonacci(n) {
-  if (!Number.isInteger(n) || n < 0) {
+  if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
